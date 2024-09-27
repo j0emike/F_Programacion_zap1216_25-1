55737cd0d312bce4a43bbb23323a68b82637f624
ES-TV <josepe0607@gmail.com>
2024-09-27 09:05:06 -0600

    C000_Jose
    Agregue operaciones aritmeticas.

--- P001_Hola_Mundo/P001_Hola_Mundo/P001_Hola_Mundo.cpp
+++ P001_Hola_Mundo/P001_Hola_Mundo/P001_Hola_Mundo.cpp
@@ -20,6 +20,7 @@ int main()
     //Tipo Nombre = Valor asignado de declaracion o inicializacion
     //Para guardar numeros enteros Reales
     int Edad = 0;
+    int n1, n2 = 0;
     //Para guardar numeros decimales
     float Altura = 0;
     float Cali = 0;
@@ -40,18 +41,18 @@ int main()
     //STD::CIN >> Variable donde se guardan los datos.
     std::cin >> Edad;
 
-    std::cout << "Y cuanto dirias que mides?\n (Dame tu respuesta en centimetros por favor)\n";
+    std::cout << "Y cuanto dirias que mides?\n";
     std::cin >> Altura;
 
-    d1 = Altura < 170;
-
-    if (d1 = true) {
+    d1 = Altura <= 1.70;
+    
+    if (d1) {
         std::cout << "Ja! Chaparro :(" << std::endl;
     }
 
     std::cout << "Sabias que una variable tipo double almacena numeros desde 2,22507*(e^-308) hasta 1,79769*(e^308)? :O" << std::endl;
 
-    std::cout << "Y cual es tu sexo?" << std::endl;
+    std::cout << "Y cual es tu sexo? M o F?" << std::endl;
     std::cin >> sexo;
 
     std::cout << "A todo esto, olvide preguntar tu nombre!" << std::endl;
@@ -61,7 +62,20 @@ int main()
     std::cout << "Entonces tienes " << Edad << " años." << std::endl;
     std::cout << "Mides " << Altura << std::endl;
     std::cout << "Tu sexo es " << sexo << std::endl;
-    std::cout << "Y te llamas " << name << " Super! Bienvenido <3" << std::endl;
-    std::cout << "Mi nombre es " << nombre;
+    std::cout << "Y te llamas " << name << ". Super! Bienvenido <3" << std::endl;
+    std::cout << "Mi nombre es " << nombre << "muito gusto." << std::endl;
+    std::cout << "Yo puedo calcular muchas cosas, que tal si me das 2 numeros y probamos." << std::endl;
+    std::cout << "Numero 1: " << std::endl;
+    std::cin >> n1;
+    std::cout << "Numero 2: " << std::endl;
+    std::cin >> n2;
+    std::cout << "Su suma seria: " << n1 + n2 << std::endl;
+    std::cout << "Su resta seria: " << n1 - n2 << std::endl;
+    std::cout << "Su multiplicacion seria " << n1 * n2 << std::endl;
+    std::cout << "Su division seria: " << n1 / n2 << std::endl;
+    std::cout << "La raiz cuadrada de " << n1 << " seria " << sqrt(n1) << std::endl;
+    std::cout << "La raiz cuadrada de " << n2 << " seria " << sqrt(n2) << std::endl;
+    std::cout << "Si elevamos al cuadrado " << n1 << " seria " << pow(n1, 2) << std::endl;
+    std::cout << "Si elevamos al cuadrado " << n2 << " seria " << pow(n2, 2) << std::endl;
 }
 
