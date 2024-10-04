f76841a6ca9d13f0dcb3fdb3775a571dca80ebb7
j0emike <josepe0607@gmail.com>
2024-10-04 07:44:13 -0600

    C001_Jose
    Relocalice la carpeta en donde esta guardada

---
+++ P002_Valor_Absoluto/P002_Valor_Absoluto/P002_Valor_Absoluto/P002_Valor_Absoluto.cpp
@@ -0,0 +1,19 @@
+// P002_Valor_Absoluto.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    float num = 0;
+    std::cout << "Buen dia, dame un numero y te dare su valor absoluto: " << std::endl;
+    std::cin >> num;
+    if (num >= 0)
+    {
+        std::cout << "Su valor abosuluto es " << num << std::endl;
+    }
+    else
+    {
+        std::cout << "Su valor absoluto es " << num * -1 << std::endl;
+    }
+}
