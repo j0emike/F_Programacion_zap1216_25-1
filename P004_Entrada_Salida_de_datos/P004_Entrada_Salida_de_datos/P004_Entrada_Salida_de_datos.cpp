8e61f8144d785c29f44a6c2f88d928d23d26463b
j0emike <josepe0607@gmail.com>
2024-10-07 07:20:52 -0600

    C001_Jose
    Cambios

---
+++ P004_Entrada_Salida_de_datos/P004_Entrada_Salida_de_datos/P004_Entrada_Salida_de_datos.cpp
@@ -0,0 +1,15 @@
+// P004_Entrada_Salida_de_datos.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+	bool op1 = false, op2 = false, op3 = false;
+	std::string nombrePaciente1 = " ", nombrePaciente2 = "";
+	char sexo = ' ';
+	int peso = 0, altura = 0;
+	float imc = 0;
+
+}
+
