16923ed397ea1c34a2a9fc94e7e262315baddf28
j0emike <josepe0607@gmail.com>
2024-10-10 22:40:39 -0600

    C001_Jose
    Cree practica 7 y corregi cosas de la 6

--- P006_Entrada_Salida_Datos_Repaso/P006_Entrada_Salida_Datos_Repaso/P006_Entrada_Salida_Datos_Repaso.cpp
+++ P006_Entrada_Salida_Datos_Repaso/P006_Entrada_Salida_Datos_Repaso/P006_Entrada_Salida_Datos_Repaso.cpp
@@ -7,13 +7,17 @@
 
 int main()
 {
+    
     setlocale(LC_ALL, "en-US");
     std::string apodo = "j0emike";
     std::string apodo_usuario;
     std::string contra = "SecurePass";
     std::string contra_usuario;
+    int opcion = 0;
+
     std::cout << "Hola usuario dame tu nickname: " << std::endl;
     std::getline(std::cin, apodo_usuario);
+
     if (apodo == apodo_usuario)
     {
         std::cout << "Para confirmar tu acceso ingresa tu contraseña: " << std::endl;
@@ -32,7 +36,7 @@ int main()
         std::cout << "No conozco a ese usuario :( " << std::endl;
 
     }
-    int opcion = 0;
+    
     std::cout << "Que es lo primero que haces al despertar?\n";
     std::cout << "1.- Tomas un baño? 2.- Desayunas? 3.- Corres porque ya se te hizo tarde? " << std::endl;
     std::cin >> opcion;
@@ -51,5 +55,64 @@ int main()
         std::cout << "Esta opcion sale de los rangos permitidos \n";
         break;
     }
+
     std::cout << "Te parece que este es un buen codigo?" << std::endl;
+    std::cout << "0.- No      1.- Si" << std::endl;
+    std::cin >> opcion;
+    if (opcion == 1)
+    {
+        std::cout << "Muchas gracias no olvides puntuarnos con 6 puntos en la tarea." << std::endl;
+    }
+    else
+    {
+        while (opcion != 1)
+        {
+            std::cout << "Te gustaria reconsiderarlo? " << std::endl;
+            std::cout << "0.- No      1.- Si" << std::endl;
+            std::cin >> opcion;
+        }
+        std::cout << "Muchas gracias no olvides puntuarnos con 6 puntos en la tarea >:)" << std::endl;
+    }
+
+    std::cout << apodo << " te envia saludos, deseas saludar de vuelta? " << std::endl;
+    std::cout << "0.- No      1.- Si" << std::endl;
+    std::cin >> opcion;
+    if (opcion == 1)
+    {
+        std::cout << " Como lo quieres saludar? " << std::endl;
+        std::cout << " 1. Chocar los puños   2. Ondear tu mano " << std::endl;
+        std::cout << " 3. Love & Peace       4. Wasaaaaa " << std::endl;
+        std::cin >> opcion;
+        switch (opcion)
+        {
+        case 1:
+            std::cout << "*Estiras tu puño esperando respuesta*" << std::endl;
+            break;
+        case 2:
+            std::cout << "*Ondeas tu mano de un lado a otro*" << std::endl;
+            break;
+        case 3:
+            std::cout << "*Levantas tus dedos como hippie*" << std::endl;
+            break;
+        case 4:
+            std::cout << "Wasaaaaa" << std::endl;
+            break;
+        default:
+            std::cout << "Crei que querias saludar a " << apodo << " :(" << std::endl;
+            break;
+        }
+        if (opcion == 4)
+        {
+            std::cout << "Wasaaaaaa" << std::endl;
+        }
+        else if (opcion == 1)
+        {
+            std::cout << "No es como que pueda chocarla, pero recibira tu saludo :) " << std::endl;
+        }
+        else
+        {
+            std::cout << ":)" << std::endl;
+        }
+    }
+    
 }
---
+++ P007_Decisiones/P007_Decisiones/P007_Decisiones.cpp
@@ -0,0 +1,39 @@
+// P007_Decisiones.cpp by Jose Peña
+//
+
+#include <iostream>
+
+int main()
+{
+    int peleadores = 0, formato = 0, peleas = 0, grupos = 0;
+    std::cout << "Dime tu numero de peleadores " << std::endl;
+    std::cin >> peleadores;
+    std::cout << "Dime el formato de pelea: " << std::endl;
+    std::cout << "1.- Royal Rumble  2.- Eliminatoria directa  3.- Grupos, Semifinal y final. " << std::endl;
+    std::cin >> formato;
+    switch (formato)
+    {
+    case 1:
+        std::cout << "El numero de combates necesario para obtener a un ganador es de " << peleadores - 1 << " combates." << std::endl;
+        break;
+    case 2:
+        std::cout << "El numero de combates necesario para obtener a un ganador es de " << peleadores - 1 << " combates." << std::endl;
+        break;
+    case 3:
+        if (peleadores % 4 == 0)
+        {
+            grupos = peleadores / 4;
+            peleas = grupos * 6;
+            peleas += (grupos * 2) - 1;
+            std::cout << "El numero de combates necesario para obtener a un ganador es de " << peleas << " combates." << std::endl;
+        }
+        else
+        {
+            std::cout << "Este formato no puede funcionar si los peleadores no forman grupos de 4." << std::endl;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
