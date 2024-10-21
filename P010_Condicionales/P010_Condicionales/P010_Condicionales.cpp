7bd2a442078e4bc9531789cd8ca27ca1b4312105
j0emike <josepe0607@gmail.com>
2024-10-21 08:32:58 -0600

    C002_Jose
    Mas cosas

--- P010_Condicionales/P010_Condicionales/P010_Condicionales.cpp
+++ P010_Condicionales/P010_Condicionales/P010_Condicionales.cpp
@@ -5,6 +5,7 @@
 #include <locale.h>
 #include <string>
 #include <regex>
+#include < ctype.h >
 
 int main()
 {
@@ -14,8 +15,7 @@ int main()
     int contra = 12345;
     int contra_usuario;
     int intentos = 3, opcion = 0;
-    bool contains_non_alpha 
-        = !std::regex_match(apodo_usuario, std::regex("^[A-Za-z]+$"));
+    bool comprobacion;
     do
     {
         std::cout << "Que deseas hacer? " << std::endl;
@@ -23,14 +23,19 @@ int main()
         std::cin >> opcion;
         switch (opcion)
         {
+        case 0:
+            std::cout << "Gracias" << std::endl;
+            break;
         case 1:
             do
             {
                 std::cout << "Hola usuario dame tu nickname: " << std::endl;
+                std::cin.ignore();
                 std::getline(std::cin, apodo_usuario);
-                
-            } while (contains_non_alpha != false);
-            
+                bool contains_non_alpha
+                    = !std::regex_match(apodo_usuario, std::regex("^[A-Za-z]+$"));
+                comprobacion = contains_non_alpha;
+            } while (comprobacion != false);
             if (apodo == apodo_usuario)
             {
                 do
@@ -40,6 +45,7 @@ int main()
                     if (contra == contra_usuario)
                     {
                         std::cout << "Bienvenido " << apodo << std::endl;
+                        intentos = 0;
                     }
                     else
                     {
@@ -47,14 +53,35 @@ int main()
                         std::cout << "Intentos restantes " << intentos - 1 << std::endl;
                         intentos--;
                     }
-                } while (intentos > 0);
+                } while (intentos > 0 && isdigit(contra_usuario)!=true);
             }
             else
             {
                 std::cout << "No conozco a ese usuario :( " << std::endl;
             }
+            break;
+        case 2:
+            do
+            {
+            std::cout << "Ingresa el nombre del usuario: " << std::endl;
+            std::cin.ignore();
+            std::getline(std::cin, apodo);
+            bool contains_non_alpha2
+                = !std::regex_match(apodo, std::regex("^[A-Za-z]+$"));
+            comprobacion = contains_non_alpha2;
+            } while (comprobacion != false);
+            do
+            {
+            std::cout << "Ingresa la contraseña: " << std::endl;
+            std::cin >> contra;
+            } while (isdigit(contra)!=1);
+            break;
+        default:
+            std::cout << "Opcion invalida " << std::endl;
+            break;
         }
-        
+        system("pause");
+        system("cls");
     } while (opcion != 0);
 }
 
