240f82452bb3a5191cfccbf7ceb1bb375b181c5a
j0emike <josepe0607@gmail.com>
2024-10-25 00:40:48 -0600

    C002_Jose
    Subi mal la P010, aqui la correcion

--- P010_Condicionales/P010_Condicionales/P010_Condicionales.cpp
+++ P010_Condicionales/P010_Condicionales/P010_Condicionales.cpp
@@ -1,11 +1,15 @@
-// P010_Condicionales.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// P010_Condicionales.cpp : by j0emike
 //
 
 #include <iostream>
 #include <locale.h>
 #include <string>
 #include <regex>
-#include < ctype.h >
+
+bool soloLetras(const std::string& frase) {
+    std::regex patron("^[A-Za-z ]+$");
+    return std::regex_match(frase, patron);
+}
 
 int main()
 {
@@ -15,7 +19,6 @@ int main()
     int contra = 12345;
     int contra_usuario;
     int intentos = 3, opcion = 0;
-    bool comprobacion;
     do
     {
         std::cout << "Que deseas hacer? " << std::endl;
@@ -23,29 +26,33 @@ int main()
         std::cin >> opcion;
         switch (opcion)
         {
-        case 0:
-            std::cout << "Gracias" << std::endl;
-            break;
         case 1:
             do
             {
                 std::cout << "Hola usuario dame tu nickname: " << std::endl;
                 std::cin.ignore();
                 std::getline(std::cin, apodo_usuario);
-                bool contains_non_alpha
-                    = !std::regex_match(apodo_usuario, std::regex("^[A-Za-z]+$"));
-                comprobacion = contains_non_alpha;
-            } while (comprobacion != false);
+                if (!soloLetras(apodo_usuario))
+                {
+                    std::cout << "El nickname solo lleva letras." << std::endl;
+                    system("pause");
+                }
+                system("cls");
+            } while (!soloLetras(apodo_usuario));
+
             if (apodo == apodo_usuario)
             {
                 do
                 {
+                    std::cout << "Hola " << apodo_usuario << std::endl;
                     std::cout << "Para confirmar tu acceso ingresa tu contraseña: " << std::endl;
                     std::cin >> contra_usuario;
                     if (contra == contra_usuario)
                     {
-                        std::cout << "Bienvenido " << apodo << std::endl;
+                        std::cout << "Bienvenido " << apodo << " presiona un boton para hacer log off " << std::endl;
                         intentos = 0;
+                        system("pause");
+                        system("cls");
                     }
                     else
                     {
@@ -53,7 +60,7 @@ int main()
                         std::cout << "Intentos restantes " << intentos - 1 << std::endl;
                         intentos--;
                     }
-                } while (intentos > 0 && isdigit(contra_usuario)!=true);
+                } while (intentos > 0);
             }
             else
             {
@@ -63,25 +70,35 @@ int main()
         case 2:
             do
             {
-            std::cout << "Ingresa el nombre del usuario: " << std::endl;
-            std::cin.ignore();
-            std::getline(std::cin, apodo);
-            bool contains_non_alpha2
-                = !std::regex_match(apodo, std::regex("^[A-Za-z]+$"));
-            comprobacion = contains_non_alpha2;
-            } while (comprobacion != false);
+                std::cout << "Hola usuario dame tu nickname: " << std::endl;
+                std::cin.ignore();
+                std::getline(std::cin, apodo_usuario);
+                if (!soloLetras(apodo_usuario))
+                {
+                    std::cout << "El nickname solo lleva letras." << std::endl;
+                    system("pause");
+                }
+                system("cls");
+            } while (!soloLetras(apodo_usuario));
             do
             {
-            std::cout << "Ingresa la contraseña: " << std::endl;
-            std::cin >> contra;
-            } while (isdigit(contra)!=1);
-            break;
-        default:
-            std::cout << "Opcion invalida " << std::endl;
+                std::cout << "Ingresa la contraseña: ";
+                if (std::cin >> contra_usuario) {
+                    break;
+                }
+                else {
+                    std::cout << "La contraseña solo pueden ser numeros" << std::endl;
+                    system("pause");
+                    std::cin.clear();
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                }
+                system("cls");
+            } while (true);
+            contra = contra_usuario;
+            apodo = apodo_usuario;
             break;
         }
-        system("pause");
-        system("cls");
+
     } while (opcion != 0);
 }
 
