503ebf34654ab9ce77cda421e2024a4da45d60cf
4 <josepe0607@gmail.com>
2024-11-08 08:31:12 -0600

    C001_Jose
    Correccion P016 para solicitar datos al usuario

--- P016_CapturaDeJuegos/P016_CapturaDeJuegos/P016_CapturaDeJuegos.cpp
+++ P016_CapturaDeJuegos/P016_CapturaDeJuegos/P016_CapturaDeJuegos.cpp
@@ -1,4 +1,4 @@
-// P016_CapturaDeJuegos.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// P016_CapturaDeJuegos.cpp : by j0emike
 //
 
 #include <iostream>
@@ -11,6 +11,8 @@ int main()
     std::string autor[3] = { "Kojima", "David Fenn", "Evan Greenwod" };
     std::string studio[3] = { "Konami", "Acid Nerve", "Free Lives" };
 
+    std::cout << "Buen dia usuario, te vengo a recomendar 3 juegos: " << std::endl;
+
     for (int i = 0; i < 3; i++)
     {
         std::cout << nombreJuegos[i] << std::endl;
@@ -19,5 +21,35 @@ int main()
         std::cout << studio[i] << std::endl;
         std::cout << std::endl;
     }
-    
+
+    //Pedir al usuario sus 3 juegos
+
+    std::cout << "Espero que te gusten, seria genial si tu tambien pudieras recomendarme 3: " << std::endl;
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << "Nombre del juego " << i << ": " << std::endl;
+        std::cin >> nombreJuegos[i];
+        std::cout << "Año de publicacion: " << std::endl;
+        std::cin >> anioPublicacion[i];
+        std::cout << "Autor: " << std::endl;
+        std::cin >> autor[i];
+        std::cout << "Estudio: " << std::endl;
+        std::cin >> studio[i];
+        std::cout << std::endl;
+    }
+
+    system("cls");
+
+    std::cout << "Tus juegos son: " << std::endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << nombreJuegos[i] << std::endl;
+        std::cout << anioPublicacion[i] << std::endl;
+        std::cout << autor[i] << std::endl;
+        std::cout << studio[i] << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Excelente, tratare de jugarlos." << std::endl;
 }
